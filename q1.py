84ae5d5fc5fdcc7d98702026c3e0da01e2ab1dfe
nivriti12 <143018878+nivriti12@users.noreply.github.com>
2026-09-15 15:57:16 -0400

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,10 @@
+
+import base64 as _b64
+import numpy as np
+
+def check_q1(mean_value, data):
+    _64 = _b64.b64decode(
+        "CmFzc2VydCBtZWFuX3ZhbHVlID09IG5wLm1lYW4oZGF0YSksICJOb3QgcXVpdGUg4oCUIHRyeSBhZ2Fpbi4iCnByaW50KCLinJMgQ29ycmVjdCEiKQo="
+    )
+    exec(_64.decode())
+
